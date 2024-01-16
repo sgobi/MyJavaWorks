63e3ccbede179c2c1f1ce85f2ccbbc75998c86bf
sgobi <g2k2@live.com>
2024-01-16 13:40:59 +0530

    Loging gui

--- StudentManagementSystem/view/login/loging.java
+++ StudentManagementSystem/view/login/loging.java
@@ -2,6 +2,6 @@ package StudentManagementSystem.view.login;
 public class loging {
 
     public static void main(String[] args) {
-        System.out.println("hi");
+        System.out.println("hgg");
     }
 }
