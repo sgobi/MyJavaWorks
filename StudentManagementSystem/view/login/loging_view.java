50490f2a999d7bb393ac85f1e59f28a25dc112d5
sgobi <g2k2@live.com>
2024-01-16 14:55:53 +0530

    Loging gui

--- StudentManagementSystem/view/login/loging_view.java
+++ StudentManagementSystem/view/login/loging_view.java
@@ -1,8 +1,13 @@
 package StudentManagementSystem.view.login;
 
+import javax.swing.*;
+import javax.swing.border.TitledBorder;
+import java.awt.*;
+
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JPanel;
 import javax.swing.JTextField;
 
 public class loging_view {
@@ -10,46 +15,71 @@ public class loging_view {
  loging_view()
 {
 
-   jf = new JFrame("Loging");
-    jf.setSize(500,250);
+
 
 }
 
 public void loging_textboxes_buttons(){
+    jf = new JFrame("Loging");
+
+    JPanel mainPanel = new JPanel();
+    mainPanel.setLayout(new BorderLayout());
+
+
+    JPanel groupBoxPanel = new JPanel();
+
+    groupBoxPanel.setLayout(new BorderLayout( )); 
+    groupBoxPanel.setBorder(new TitledBorder("Loging"));
+     groupBoxPanel.setPreferredSize(new Dimension(400, 150));
+
+
+    JPanel attributes_grid = new JPanel();
+
+    attributes_grid.setLayout(new GridLayout());
 
 JLabel lbl_u_name = new JLabel();
 lbl_u_name.setText("User Name");
-lbl_u_name.setBounds(100, 50,150, 30);
+// lbl_u_name.setBounds(100, 50,150, 30);
+
+JTextField jtf_u_name = new JTextField();
+// jtf_u_name.setBounds(200, 50,150, 30);
 
 JLabel lbl_password = new JLabel();
 lbl_password.setText("Password");
-lbl_password.setBounds(100, 100, 150, 30);
-
-JTextField jtf_u_name = new JTextField();
-jtf_u_name.setBounds(200, 50,150, 30);
+// lbl_password.setBounds(100, 100, 150, 30);
 
 JTextField jtf__password = new JTextField();
-jtf__password.setBounds(200, 100, 150, 30);
+// jtf__password.setBounds(200, 100, 150, 30);
 
     JButton btn_loging = new JButton();
     btn_loging.setText("Loging");
-    btn_loging.setBounds(100, 150, 150, 30);
+    // btn_loging.setBounds(100, 150, 150, 30);
 
     JButton btn_exit = new JButton();
     btn_exit.setText("Exit");
-    btn_exit.setBounds(300, 150, 150, 30);
+    // btn_exit.setBounds(300, 150, 150, 30);
+
+    JPanel groupBoxPanel2 = new JPanel();
+    groupBoxPanel2.setLayout(new BorderLayout()); 
 
 
+    groupBoxPanel2.add(lbl_u_name);
+    groupBoxPanel2.add(jtf_u_name);
+    groupBoxPanel2.add(lbl_password);
+    groupBoxPanel2.add(jtf__password);
+    groupBoxPanel2.add(btn_loging);
+    groupBoxPanel2.add(btn_exit);
+groupBoxPanel2.setLayout(new GridLayout(3, 2));
 
-    jf.add(lbl_u_name);
-    jf.add(jtf_u_name);
-    jf.add(lbl_password);
-    jf.add(jtf__password);
-    jf.add(btn_loging);
-    jf.add(btn_exit);
+groupBoxPanel.add(groupBoxPanel2);
 
-    jf.setLayout(null);
+    mainPanel.add(groupBoxPanel, BorderLayout.CENTER);
+
+    jf.setContentPane(mainPanel);
+  //  jf.setSize(500,250);
+    jf.pack();
     jf.setVisible(true);
+    jf.setLocation(500,250);
 }
 
     public static void main(String[] args) {
